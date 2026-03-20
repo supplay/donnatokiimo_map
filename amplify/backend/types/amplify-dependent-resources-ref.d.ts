bf0e51e17e15d229325437e9dfc105ba234ace51
BELL4287\suppl <supplay/kt9@gmail.com>
2026-03-20 16:28:36 +0900

    fix: フォルダ構造を整理し、リポジトリを再構築

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "donnatokiimomapfbb27a9d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3efcda499": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
