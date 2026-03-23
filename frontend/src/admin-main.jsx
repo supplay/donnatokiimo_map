05593ac6817513cb8cc281b90b9bda848750c42d
BELL4287\suppl <supplay/kt9@gmail.com>
2026-03-23 15:08:06 +0900

    Add amplify.yml for Gen 2

--- frontend/src/admin-main.jsx
+++ frontend/src/admin-main.jsx
@@ -2,21 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
+
 import { Amplify } from "aws-amplify";
-import awsmobile from "../../src/aws-exports";
+import outputs from "../../amplify_outputs.json";
 import App from "./App.jsx";
 
-Amplify.configure({
-  ...awsmobile,
-  Storage: {
-    S3: {
-      bucket: awsmobile.aws_user_files_s3_bucket,
-      region: awsmobile.aws_user_files_s3_bucket_region
-    }
-  }
-});
-
-console.log("設定されたバケット(admin):", Amplify.getConfig().Storage?.S3?.bucket);
+Amplify.configure(outputs);
 
 // PWA でホーム画面から /admin/ で起動したとき /kanri_aki へ移動
 if (!window.location.pathname.startsWith("/kanri_aki")) {
